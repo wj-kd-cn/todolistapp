ab1fd274b8346c75e7863f792c1ffd4dc25a1943
wangjun0054 <40479477+wangjun0054@users.noreply.github.com>
2023-10-15 17:26:51 +0800

    声明APP的类组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,45 @@
-
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="todo-container">
-      <div className="todo-wrap">
-
-        <div className="todo-header">
-          <input type="text" placeholder="请输入你的任务名称，按回车确认"/>
+export default class App extends Component {
+  render() {
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+  
+          <div className="todo-header">
+            <input type="text" placeholder="请输入你的任务名称，按回车确认"/>
+          </div>
+  
+          <ul className="todo-main">
+            <li>
+              <label>
+                <input type="checkbox" />
+                <span>xxxxxx</span>
+                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+              </label>
+            </li>
+            <li>
+              <label>
+                <input type="checkbox" />
+                <span>yyyyy</span>
+                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+              </label>
+            </li>
+  
+          </ul>
+          <div className="todo-footer">
+            <label> <input type="checkbox" /></label>
+            <span><span>已完成0</span>/ 全部2</span>
+            <button className="btn btn-danger">清除已完成的任务</button>
+          </div>
+  
         </div>
-
-        <ul className="todo-main">
-          <li>
-            <label>
-              <input type="checkbox" />
-              <span>xxxxxx</span>
-              <button className="btn btn-danger" style={{display:'none'}}>删除</button>
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" />
-              <span>yyyyy</span>
-              <button className="btn btn-danger" style={{display:'none'}}>删除</button>
-            </label>
-          </li>
-
-        </ul>
-        <div className="todo-footer">
-          <label> <input type="checkbox" /></label>
-          <span><span>已完成0</span>/ 全部2</span>
-          <button className="btn btn-danger">清除已完成的任务</button>
-        </div>
-
+  
+  
       </div>
+    )
 
-
-    </div>
-
-  );
+  }
 }
 
-export default App;
