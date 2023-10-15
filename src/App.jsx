50a40ced31a5138a9cc5ded4d3dc103e30828f0d
wangjun0054 <40479477+wangjun0054@users.noreply.github.com>
2023-10-15 17:18:40 +0800

    app页面开发

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
-import Footer from './components/Footer'
-import Header from './components/Header'
-import List from './components/List'
+
 import './App.css';
 
 function App() {
   return (
-    <div class="todo-container">
-      <div class="todo-wrap">
-        <div class="todo-header">
-          <input type="text" plaschoder="请输入你的任务名称，按回车确认"/>
+    <div className="todo-container">
+      <div className="todo-wrap">
 
+        <div className="todo-header">
+          <input type="text" placeholder="请输入你的任务名称，按回车确认"/>
         </div>
-        <ul class="todo-main">
+
+        <ul className="todo-main">
           <li>
             <label>
               <input type="checkbox" />
               <span>xxxxxx</span>
-              <button class="btn btn-danger" style="display:none">删除</button>
+              <button className="btn btn-danger" style={{display:'none'}}>删除</button>
             </label>
           </li>
           <li>
             <label>
               <input type="checkbox" />
               <span>yyyyy</span>
-              <button class="btn btn-danger" style="display:none">删除</button>
+              <button className="btn btn-danger" style={{display:'none'}}>删除</button>
             </label>
           </li>
 
         </ul>
-        <div class="todo-footer">
+        <div className="todo-footer">
           <label> <input type="checkbox" /></label>
           <span><span>已完成0</span>/ 全部2</span>
-          <button class="btn btn-danger">清除已完成的任务</button>
+          <button className="btn btn-danger">清除已完成的任务</button>
         </div>
 
       </div>
