a938bba105859d3b43b0f61b6d699a1becf3586e
wangjun0054 <40479477+wangjun0054@users.noreply.github.com>
2023-10-29 17:56:40 +0800

    获取checkbox的勾选信息，并同步更新到状态中

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -8,20 +8,29 @@ export default class Item extends Component {
 
   //在指定事件回调的时候，传入参数，则返回必须是个函数
 
-  handleMouse =(falg) =>{
+  handleMouse = (falg) =>{
     return () =>{
       this.setState({mouse:falg})
     }
   }
+
+  //获取checkbox的勾选信息，并同步更新到状态中
+
+  handleCheck = (sid) => {
+    return (event) => {
+      console.log(sid,event.target.checked);
+
+    }
+  }
   render() {
     //console.log('@item: ',this);
-    const {sname,sdone} = this.props
+    const {sid,sname,sdone} = this.props
     const {mouse} =  this.state
     return (
     <li style={{background:mouse ? '#ddd' :'white'}} onMouseLeave={this.handleMouse(false)} onMouseEnter={this.handleMouse(true)}>
       <label>
         {/* defaultChecked属性可支持默认别勾选，页面可更改 */}
-        <input type="checkbox" defaultChecked={sdone} />
+        <input type="checkbox" defaultChecked={sdone} onChange={this.handleCheck(sid)} />
         <span>{sname}</span>
       </label>
       {/* 增加删除按钮的鼠标滑动显示*/}
